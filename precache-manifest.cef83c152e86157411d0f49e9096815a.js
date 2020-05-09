693842dcd6c4fab4f1375626bb993cfd72387729
TrueYork <vasy7exit@gmail.com>
2020-05-09 11:32:57 +0300

    Updates

---
+++ precache-manifest.cef83c152e86157411d0f49e9096815a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d103554aa8992c8cf0f13dac5d8ac9",
+    "url": "/react1/index.html"
+  },
+  {
+    "revision": "67656db7ca8a15e5192b",
+    "url": "/react1/static/css/3.d9bc1231.chunk.css"
+  },
+  {
+    "revision": "56b4c59fb2fef7f052c4",
+    "url": "/react1/static/css/4.1fa9c706.chunk.css"
+  },
+  {
+    "revision": "c7a6ef86f51084f8eef1",
+    "url": "/react1/static/css/main.afbb8737.chunk.css"
+  },
+  {
+    "revision": "2900263633e754c078cb",
+    "url": "/react1/static/js/2.2b9985e3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react1/static/js/2.2b9985e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67656db7ca8a15e5192b",
+    "url": "/react1/static/js/3.9d0c6995.chunk.js"
+  },
+  {
+    "revision": "56b4c59fb2fef7f052c4",
+    "url": "/react1/static/js/4.3073ef6b.chunk.js"
+  },
+  {
+    "revision": "c7a6ef86f51084f8eef1",
+    "url": "/react1/static/js/main.15920016.chunk.js"
+  },
+  {
+    "revision": "2dbdc5c28d65332cea03",
+    "url": "/react1/static/js/runtime-main.90ac77aa.js"
+  },
+  {
+    "revision": "27fc27b682d7147fc0935299c8467922",
+    "url": "/react1/static/media/loaderTransparentRed.27fc27b6.svg"
+  },
+  {
+    "revision": "db0469234d3b806eda66a151ac198647",
+    "url": "/react1/static/media/userPicDefault.db046923.png"
+  }
+]);
